f70fe1d27400be64cad365dd16f321ded7d9c67a
Lab2-16 <Lab2-16@LAB2-16>
2025-03-15 04:11:23 -0600

    Agregar archivos de proyecto.

---
+++ Acciones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arbol
+{
+    internal class Acciones
+    {
+        private List<Alumno> lista = new List<Alumno>();
+        Alumno a = new Alumno();
+
+        public void AddStudent()
+        {
+            lista.Add(new Alumno(a.Nombre,a.Edad,a.Genero,a.Matricula,a.Carrera));
+        }
+    }
+}
---
+++ Alumno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arbol
+{
+    class Alumno : Persona 
+    {
+        public Alumno () { }
+
+        public Alumno(string nombre, int edad, string genero, int matricula, string carrera)
+        :base(nombre, edad, genero)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Genero = genero;
+            Matricula = matricula;
+            Carrera = carrera;
+        }
+
+        public int Matricula {  get; set; }
+        public string Carrera { get; set; }
+    }
+}
